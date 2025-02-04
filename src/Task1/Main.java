f0c82b0412a51ef9308c5510ac82fb4da484b5d8
ainura14 <anusubalieva141104@gmail.com>
2025-02-04 11:28:22 +0600

    first commit

---
+++ src/Task1/Main.java
@@ -0,0 +1,12 @@
+package Task1;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("""
+                        J        a     v       v    a
+                        J      a   a   v     v   a   a
+                        J     J    aaaaa   v  v   aaaaa
+                        J J    a          a   v   a           a
+                """);
+    }
+}
