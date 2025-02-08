ea37e50c15452544358c72a3496cbb6c923f5e12
ainura14 <anusubalieva141104@gmail.com>
2025-02-08 20:38:15 +0600

    have used random

---
+++ src/Task1/OneMethod/RandomTask.java
@@ -0,0 +1,8 @@
+package Task1.OneMethod;
+
+public class Random {
+    public static void main(String[] args) {
+        java.util.Random random = new java.util.Random();
+        random.nextInt()
+    }
+}
