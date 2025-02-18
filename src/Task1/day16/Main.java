d54bb0f7f8dda6a99010f63a0c0ce367f24372b0
ainura14 <anusubalieva141104@gmail.com>
2025-02-18 15:23:40 +0600

    day16

---
+++ src/Task1/day16/Main.java
@@ -0,0 +1,11 @@
+package Task1.day16;
+
+public class Main {
+    public void sendMessage(String message) {
+        System.out.println(message);
+    }
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.sendMessage("Hello World");
+    }
+}
