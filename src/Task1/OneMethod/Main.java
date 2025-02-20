c8e13a72c333cfabf182517137a51666ebe577a2
ainura14 <anusubalieva141104@gmail.com>
2025-02-20 16:08:45 +0600

    day 18

--- src/Task1/OneMethod/Main.java
+++ src/Task1/OneMethod/Main.java
@@ -1,4 +1,4 @@
-package Task1;
+package Task1.OneMethod;
 
 public class Main {
     public static void main(String[] args) {
---
+++ src/Task1/day18/Main.java
@@ -0,0 +1,13 @@
+package Task1.day18;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String input;
+        Scanner scanner = new Scanner(System.in);
+        input = scanner.nextLine();
+        System.out.println(input);
+        scanner.close();
+    }
+}
