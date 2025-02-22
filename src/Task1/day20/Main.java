9a8cc05d578c7bd67092088055df889ac67e727f
ainura14 <anusubalieva141104@gmail.com>
2025-02-22 16:42:06 +0600

    day 20

---
+++ src/Task1/day20/Main.java
@@ -0,0 +1,12 @@
+package Task1.day20;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        scanner.close();
+        System.out.println(input);
+    }
+}
