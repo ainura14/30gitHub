52d0d4a7a6bdcce89ed094d6817fcc4fc84f5065
ainura14 <anusubalieva141104@gmail.com>
2025-03-04 14:56:53 +0600

    day 30 last

---
+++ src/Task1/day30/Main.java
@@ -0,0 +1,10 @@
+package Task1.day30;
+
+public class Main {
+    public static void main(String[] args) {
+        String inputFile = "input.txt";
+        String outputFile = "output.txt";
+        System.out.println("Input file: " + inputFile);
+        System.out.println("Output file: " + outputFile);
+    }
+}
